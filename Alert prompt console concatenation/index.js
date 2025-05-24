b891b9e27aec753ab48a4efa620f5bbfa163b043
TariqAliBaluch <tariqali73873@gmail.com>
2025-05-24 20:54:22 +0500

    question added

--- Alert prompt console concatenation/index.js
+++ Alert prompt console concatenation/index.js
@@ -1,4 +1,11 @@
 //Assignment 1: Welcome Message
 
-var username = prompt("What is your username?");
-alert("Welcom, " + username);
+// var username = prompt("What is your username?");
+// alert("Welcom, " + username);
+
+//Assignment 2: Age Message
+
+// var username = prompt("what is your name?");
+// var age = prompt("what is your age?");
+
+// console.log(username + " is " + age +" years old" );
