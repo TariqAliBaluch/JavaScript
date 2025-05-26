09b716c3ca86fdc33ccc2f6ac4bda5a59af61538
TariqAliBaluch <tariqali73873@gmail.com>
2025-05-26 12:34:25 +0500

    questions added

--- Alert prompt console concatenation/index.js
+++ Alert prompt console concatenation/index.js
@@ -9,3 +9,66 @@
 // var age = prompt("what is your age?");
 
 // console.log(username + " is " + age +" years old" );
+
+// Assignment 3: Favorite Color
+
+// var color = prompt("what is your favorite color?");
+// alert("Wow! " + color + " is a nice color!");
+
+//Assignment 4: Simple Math Sentence
+
+// var num1 = prompt("Enter first number");
+// var num2 = prompt("Enter second number");
+
+// var result = Number(num1) + Number(num2);
+
+// console.log("The result of " + num1 + "+" + num2 + " is " + result );
+
+
+//Assignment 5: About Me Summary
+
+// var username = prompt("What is Your Name?");
+// var country = prompt("What is your Country?");
+// var hobby = prompt("What is your Hobby?");
+
+// alert("Hi, I am " + username + " from " + country + ", and I love " + hobby);
+
+//Task 1: Create a Full Address
+
+// var street = prompt("Enter street name and number");
+// var city = prompt("Enter city name");
+// var country = prompt("Enter country name");
+
+// alert("Your full address is: " + street + ", " + city + ", " + country);
+
+//Task 2: Make a Custom Username
+
+// var username = prompt("Enter usename");
+// var number = prompt("Enter Favorite number");
+
+// var result = username + number;
+
+// alert("Your new Username is: " + result);
+
+
+//Task 3: Email Generator
+
+// var firstName = prompt("Enter your first name");
+// var lastName = prompt("Enter your last name");
+
+// alert(firstName + "." + lastName + "@gmail.com")
+
+//Task 4: Combine Favorite Things
+
+// var color = prompt("Enter your favorite color");
+// var animal = prompt("Enter your favorite animal");
+// var food = prompt("Enter your favorite food");
+
+// alert("Your favorite things are: " + color +" " + animal + " and " + food);
+
+//Task 5: Quote with Author
+
+var quote = prompt("Write a quotation");
+var authorName = prompt("Write author name");
+
+alert(quote + " - " + authorName)
