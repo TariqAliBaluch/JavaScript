b545e2f3492ec0179c5c529e3f44616c55c52d72
TariqAliBaluch <tariqali73873@gmail.com>
2025-05-31 12:48:55 +0500

    condition

---
+++ 03-conditional-statements/app.js
@@ -0,0 +1,21 @@
+// 1. Ask the user for their age. If the age is 18 or more, show an alert: "You are an
+// adult.", else show: "You are a minor."
+
+
+// var age = prompt("enter your age");
+
+// if (age >= 18) {
+//     alert("you are an adult");
+// } else{
+//     alert("you are a minor");
+// }
+
+
+// 2. Ask the user to enter a number. If it's positive, log: "Positive number"; else log:
+// "Not positive".
+
+var num =  parseInt(prompt("enter a number"));
+    console.log(typeof num);
+    
+
+
