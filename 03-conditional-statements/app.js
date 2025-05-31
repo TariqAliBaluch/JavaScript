6d3b509d8f41858d682e8ff5e22cceba96b1fecd
TariqAliBaluch <tariqali73873@gmail.com>
2025-05-31 15:56:46 +0500

    question added

--- 03-conditional-statements/app.js
+++ 03-conditional-statements/app.js
@@ -1,7 +1,6 @@
 // 1. Ask the user for their age. If the age is 18 or more, show an alert: "You are an
 // adult.", else show: "You are a minor."
 
-
 // var age = prompt("enter your age");
 
 // if (age >= 18) {
@@ -11,11 +10,59 @@
 // }
 
 
+
 // 2. Ask the user to enter a number. If it's positive, log: "Positive number"; else log:
 // "Not positive".
 
-var num =  parseInt(prompt("enter a number"));
-    console.log(typeof num);
+// var num =  prompt("enter a number");
     
+// if (num >= 0) {
+//     console.log("positive number");
+// } else{
+//     console.log("Not positive");  
+// }
+
+
+
+// 3. Ask the user for a number. If it’s even, alert: "Even number"; else alert: "Odd 
+// number". 
+
+// var num = prompt("enter a number");
+
+// if (num % 2 === 0) {
+//     alert("even number");
+// } else{
+//     alert("odd number");
+// }
+
+
+
+// 4. Ask for a username. If the username is "admin", display "Welcome, admin!"; else 
+// display "Access denied." 
+
+// var username = prompt("enter your username");
+
+// if (username == 'admin') {
+//     alert("Welcome, admin!");
+// } else{
+//     alert("Acess denied");
+// }
+
+
+// 5. Ask for two numbers. If they are equal, log "Both numbers are the same"; 
+// otherwise, log "The numbers are different". 
+    
+// var num1 = prompt("enter first number");
+// var num2 = prompt("enter second number");
+
+// if (num1 == num2) {
+//     console.log("Both numbers are the same");
+// } else{
+//     console.log("The numbers are different");
+// }
+
+
+
+
 
 
