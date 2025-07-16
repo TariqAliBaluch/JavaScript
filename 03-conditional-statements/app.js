842d25ca2f15aaed9e9374e92c012ea70eb239c8
TariqAliBaluch <tariqali73873@gmail.com>
2025-07-16 17:01:08 +0500

    added

--- 03-conditional-statements/app.js
+++ 03-conditional-statements/app.js
@@ -126,6 +126,125 @@
 // }
 
 
+// 11. Ask the user for their age. If it's 60 or more, log "You are a senior citizen.", 
+// else log "You are not a senior."
+
+// var age = prompt("Enter your age");
+
+// if (age >= 60) {
+//     console.log("You are a senior citizen");
+    
+// } else {
+//     console.log("You are not a senior citizen");
+// }
+
+// 12. Ask the user for their name. If the name is "Alice", alert "Hi, Alice!"; else alert 
+// "You're not Alice!"
+
+// var username = prompt("What is your name?");
+
+// if (username == "Alice") {
+//     alert("Hi, Alice");
+// } else {
+//     alert("You're not Alice");
+// }
+
+
+// 13. Ask the user for a password. If it matches "12345", log "Correct password"; else 
+// log "Wrong password".
+
+// var pass = prompt("Enter the password");
+
+// if (pass == 12345) {
+//     console.log("Correct password");
+    
+// } else{
+//     console.log("Wrong password");
+    
+// }
+
+
+// 14. Ask the user for the current hour (0–23). If it’s less than 12, alert "Good morning!"; 
+// else alert "Good afternoon!" 
+
+// var currentHour = prompt("Enter the current hour");
+
+// if (currentHour < 12) {
+//     alert("Good Morning!")
+// } else {
+//     alert("Good afternoon!")
+// }
+
+// 15. Ask the user to enter a number. If it's divisible by 5, log "Divisible by 5"; else log 
+// "Not divisible by 5". 
+
+// var num = prompt("enter a number");
+
+// if (num % 5 === 0) {
+//     console.log("Divisible by 5");
+    
+// }else {
+//     console.log("Not divisible by 5");
+    
+// }
+
+// 16. Ask the user if they want to play a game (yes/no). If they type "yes", alert "Let's 
+// play!"; else alert "Maybe next time!" 
+
+// var game = prompt("Do you want to play a game ?");
+
+// if (game == "yes") {
+//     alert("Let's play!")
+// } else {
+//     alert("Maybe next time!")
+// }
+
+// 17. Ask the user for a color. If the color is "blue", log "Cool choice!"; else log 
+// "Interesting color!" 
+
+// var color = prompt("what is your favorite color?");
+
+// if (color == 'blue') {
+//     console.log("Cool choice!");
+    
+// } else {
+//     console.log("Interesting color!");
+    
+// }
+
+
+// 18. Ask for the day of the week. If it’s "Sunday", alert "Weekend!"; else alert "Weekday". 
+
+// var days = prompt("What is the day today?");
+
+// if (days == 'sunday') {
+//     alert("Weekend!")
+// } else {
+//     alert("Weekday!")
+// }
+
+
+// 19. Ask the user to enter a fruit. If it's "banana" or "apple", alert "We have that!"; 
+// else alert "Sorry, out of stock."
+
+// var fruits = prompt("which fruit do you want?");
+
+// if (fruits == 'banana' || fruits =='apple') {
+//     alert("We have that!")
+// } else {
+//     alert("Sorry, out of stock")
+// }
+
+// 20. Ask the user to type "start" to begin. If they do, alert "Game started!"; else alert 
+// "Waiting for input..." 
+
+var startGame = prompt("Type start to begin the game!");
+
+if (startGame == 'start') {
+    alert("Game started!")
+} else {
+    alert("Waiting for input...")
+}
 
 
 
